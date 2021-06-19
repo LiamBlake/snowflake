edb3f8e81324034670b62eb66bee7b455cdeb3f3
LiamBlake <liamlemblake@gmail.com>
2021-06-19 10:20:57 +0930

    set working directory in app

--- R/deploy.R
+++ R/deploy.R
@@ -3,4 +3,8 @@ install.packages("rsconnect")
 rsconnect::setAccountInfo(name="liamblake", 
                           token = Sys.getenv("RSHINY_TOKEN"),
                           secret = Sys.getenv("RSHINY_SECRET"))
+
+# Ensure that the correct working directory is set
+setwd(getSrcDirectory()[1])
+
 rsconnect::deployApp("app.R", appName="snowflake")
