e29a86bdfa3295d03c20cb8dcdd76506d25b9f13
LiamBlake <liamlemblake@gmail.com>
2021-06-19 10:28:00 +0930

    use dir instead of file

--- R/deploy.R
+++ R/deploy.R
@@ -4,4 +4,4 @@ rsconnect::setAccountInfo(name="liamblake",
                           token = Sys.getenv("RSHINY_TOKEN"),
                           secret = Sys.getenv("RSHINY_SECRET"))
 
-rsconnect::deployApp("R/app.R", appName="snowflake")
+rsconnect::deployApp("R", appName="snowflake")
