248a58a100622ad88a60ca925536c98e171c76b0
LiamBlake <liamlemblake@gmail.com>
2021-07-10 17:45:48 +0930

    Use reactive value to perform loop

--- R/app.R
+++ R/app.R
@@ -2,7 +2,7 @@ library(shiny)
 
 source("growth.R")
 
-cstate <- initialise(0.1)
+cstate <- initialise(0.9)
 
 # Define UI for application
 ui <- fluidPage(
@@ -14,24 +14,32 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       "Parameters",
-      sliderInput("a",
+      sliderInput(
+        "a",
         "Rate of water transfer:",
         min = 0,
-        max = 1,
-        value = 0.1
+        max = 10,
+        value = 1
       ),
-      sliderInput("k",
+      sliderInput(
+        "k",
         "Initial water:",
         min = 0,
         max = 1,
-        value = 0.1
+        value = 0.9
       ),
-      sliderInput("gamma",
+      sliderInput(
+        "gamma",
         "Added water:",
         min = 0,
         max = 1,
-        value = 0.1
+        value = 0.01
       ),
+      checkboxInput(
+        "show_values",
+        "Show water vapour levels",
+        value = FALSE
+      )
     ),
 
     # Show a plot of the generated distribution
@@ -43,70 +51,37 @@ ui <- fluidPage(
   )
 )
 
-# Helper function to create the polygon for each hexagon
-hexagon <- function(x, y, unitcell = 1, col = col) {
-  polygon(c(
-    x, x, x + unitcell / 2, x + unitcell, x + unitcell,
-    x + unitcell / 2
-  ), c(
-    y + unitcell * 0.125,
-    y + unitcell * 0.875,
-    y + unitcell * 1.125,
-    y + unitcell * 0.875,
-    y + unitcell * 0.125,
-    y - unitcell * 0.125
-  ),
-  col = col, border = "grey"
-  )
-}
-
-plot_cells <- function(state) {
-  return(renderPlot(
-    {
-      frozen_cells <- is_frozen(state)
-
-      x <- as.vector(frozen_cells)
-
-      nrows <- nrow(frozen_cells)
-      ncols <- ncol(frozen_cells)
-
-      # Initiate the plot window
-      plot(0, 0,
-        type = "n", axes = FALSE, xlim = c(0, ncols),
-        ylim = c(0, nrows), xlab = "", ylab = "", asp = 1
-      )
-
-      color_codes <- rep("#FFFFFF", length(x))
-      for (i in 1:length(x)) {
-        if (x[i]) color_codes[i] <- "#3288BD"
-      }
-
-      offset <- 0.5 # offset for the hexagons when moving up a row
-      for (row in 1:nrows) {
-        for (column in 0:(ncols - 1)) {
-          hexagon(column + offset, row - 1, col = color_codes[row + nrows * column])
-        }
-        offset <- ifelse(offset, 0, 0.5)
-      }
-    },
-    width = 500,
-    height = 500
-  ))
-}
-
 # Define server logic
 server <- function(input, output) {
   # Initial plot
   output$snowflake <- plot_cells(cstate)
 
+  # Approach to get iteration in app, from
+  # https://stackoverflow.com/a/61867175
+  rv <- reactiveValues(loop = 0)
+
   observeEvent(input$do, {
-    # Reset snowflake initialisation
-    cstate <- initialise(input$k)
-    
-    # Trigger the growth process
-    while (!check_stop(cstate)) {
-      cstate <- step(input$a, input$gamma, cstate)
-      output$snowflake <- plot_cells(cstate)
+    # Reset and start growth loop
+    # Don't reset unless the loop is not currently in progress
+    if (rv$loop < 1) {
+      cstate <- initialise(0.9)
+      rv$loop <- 1
+    }
+  })
+
+  # The actual loop
+  observeEvent(rv$loop, {
+    if (rv$loop > 0) {
+      # Check for stopping condition
+      if (check_stop(cstate)) {
+        rv$loop <- 0
+      } else {
+
+        # Iterate, plot
+        cstate <- step(input$a, input$gamma, cstate)
+        output$snowflake <- plot_cells(cstate)
+        rv$loop <- rv$loop + 1
+      }
     }
   })
 }
--- R/growth.R
+++ R/growth.R
@@ -1,4 +1,4 @@
-initialise <- function(k, nrow = 101, ncol = 101) {
+initialise <- function(k, nrow = 5, ncol = 5) {
   state <- matrix(k, nrow = nrow, ncol = ncol)
 
   # Frozen core
@@ -36,7 +36,14 @@ is_receptive <- function(state) {
         # This call returns a 0-length vector if the cell is
         # outside the boundaries of the array. R is weird with
         # matrices.
-        cell_is_frozen <- tryCatch({frozen[r + p[0], c + p[1]]}, error=function(cond){FALSE})
+        cell_is_frozen <- tryCatch(
+          {
+            frozen[r + p[0], c + p[1]]
+          },
+          error = function(cond) {
+            FALSE
+          }
+        )
         if (length(cell_is_frozen) > 0 && cell_is_frozen) {
           receptive[r, c] <- TRUE
           break
@@ -52,8 +59,10 @@ step <- function(a, gamma, state) {
   print("Iterating...")
   receptive <- is_receptive(state)
 
+  new_state <- state
+
   # Receptive cells
-  state[receptive] <- state[receptive] + gamma
+  new_state[receptive] <- state[receptive] + gamma
 
   # Non-receptive cells
   for (r in 1:nrow(state)) {
@@ -69,13 +78,20 @@ step <- function(a, gamma, state) {
       for (p in neighbours) {
         # Ignore cells outside of the boundary
         # As above, R is weird.
-        add <- tryCatch({state[r + p[0], c + p[1]]}, error=function(cond){0})
+        add <- tryCatch(
+          {
+            state[r + p[0], c + p[1]]
+          },
+          error = function(cond) {
+            0
+          }
+        )
         sum <- sum + ifelse(length(add) > 0, add, 0)
       }
-      state[r, c] <- (1 - a) * state[r, c] + a / 6 * sum
+      new_state[r, c] <- (1 - a) * state[r, c] + a / 6 * sum
     }
   }
-  
+
   return(state)
 }
 
@@ -94,4 +110,55 @@ check_stop <- function(state) {
   }
 
   return(FALSE)
+}
+
+# Helper function to create the polygon for each hexagon
+hexagon <- function(x, y, unitcell = 1, col) {
+  polygon(c(
+    x, x, x + unitcell / 2, x + unitcell, x + unitcell,
+    x + unitcell / 2
+  ), c(
+    y + unitcell * 0.125,
+    y + unitcell * 0.875,
+    y + unitcell * 1.125,
+    y + unitcell * 0.875,
+    y + unitcell * 0.125,
+    y - unitcell * 0.125
+  ),
+  col = col, border = "grey"
+  )
+}
+
+plot_cells <- function(state, show_values=FALSE) {
+  return(renderPlot(
+    {
+      frozen_cells <- is_frozen(state)
+
+      x <- as.vector(frozen_cells)
+
+      nrows <- nrow(frozen_cells)
+      ncols <- ncol(frozen_cells)
+
+      # Initiate the plot window
+      plot(0, 0,
+        type = "n", axes = FALSE, xlim = c(0, ncols),
+        ylim = c(0, nrows), xlab = "", ylab = "", asp = 1
+      )
+
+      color_codes <- rep("#FFFFFF", length(x))
+      for (i in 1:length(x)) {
+        if (x[i]) color_codes[i] <- "#3288BD"
+      }
+
+      offset <- 0.5 # offset for the hexagons when moving up a row
+      for (row in 1:nrows) {
+        for (column in 0:(ncols - 1)) {
+          hexagon(column + offset, row - 1, col = color_codes[row + nrows * column])
+        }
+        offset <- ifelse(offset, 0, 0.5)
+      }
+    },
+    width = 500,
+    height = 500
+  ))
 }
